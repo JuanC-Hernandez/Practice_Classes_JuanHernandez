cb2acef6d37432a70691dcf32effb9a258d4da92
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-15 23:31:05 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice_Classes_JuanHernandez
+{
+    internal class Program
+    {
+        // Juan Hernandez
+        // Practice Assignment: Classes
+        // 01/15/24
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
